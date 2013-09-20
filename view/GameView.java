c1737a7a122d14c7cf89602379b8977aef6d0dee
Marco Berger <lostchall@gmail.com>
2013-09-20 15:25:00 +0200

    test

--- view/GameView.java
+++ view/GameView.java
@@ -2,15 +2,15 @@ package view;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLabel;
-
+import model.*;
 
 public class GameView extends JFrame {
 
 	public static void main(String args[]) {
-		model.Game game = new model.Game();
+		Game game = new Game();
 		new GameView(game);
 	}
-	GameView(model.Game game) {
+	GameView(Game game) {
 		JLabel jlbHelloWorld = new JLabel("Sokobaaaaaaaaan");
 		add(jlbHelloWorld);
 		this.setSize(game.getSizeX(), game.getSizeY());

5ff39f8b760b65ad805ef868bc4a0512afc72905
Marco Berger <lostchall@gmail.com>
2013-09-27 15:41:53 +0200

    -

---
+++ src/ch/bfh/ti/projekt1/sokoban/model/Level.java
@@ -0,0 +1,18 @@
+package ch.bfh.ti.projekt1.sokoban.model;
+
+public class Level {
+	
+	public String getLevel1(){
+		String level = "###########\n"+
+			           "#         #\n"+
+			           "#     # ##\n"+
+			           "####    ###\n"+
+			           "###########\n";
+		return level;
+	}
+	
+	public void loadLevel(){
+		
+	}
+
+}
--- src/ch/bfh/ti/projekt1/sokoban/view/GameView.java
+++ src/ch/bfh/ti/projekt1/sokoban/view/GameView.java
@@ -1,9 +1,8 @@
 package ch.bfh.ti.projekt1.sokoban.view;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLabel;
-import ch.bfh.ti.projekt1.sokoban.model.*;
-import ch.bfh.ti.projekt1.sokoban.controller.*;
+
+import ch.bfh.ti.projekt1.sokoban.model.Game;
 
 public class GameView extends JFrame {
 
@@ -15,10 +14,10 @@ public class GameView extends JFrame {
 		JLabel jlbSokoban = new JLabel("Sokobaaaaaaaaan");
 		add(jlbSokoban);
 
-        SokoComponent sokoComponent = new SokoComponent();
-        sokoComponent.setBounds(100,100,100,100);
+        //SokoComponent sokoComponent = new SokoComponent();
+        //sokoComponent.setBounds(100,100,100,100);
 
-        add(sokoComponent);
+        //add(sokoComponent);
 		this.setSize(game.getSizeX(), game.getSizeY());
 		setVisible(true);
 	}
